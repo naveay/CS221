6364e153365deb09a34a9fb4b55f7977938ecdd4
XJ12 <biolxj12@gmail.com>
2014-02-05 15:44:08 -0800

    remove query

--- Web Crawling/src/org/webcrawling/MyCrawler.java
+++ Web Crawling/src/org/webcrawling/MyCrawler.java
@@ -16,25 +16,40 @@ import edu.uci.ics.crawler4j.url.WebURL;
 public class MyCrawler extends WebCrawler {
 
 	public static CrawlStat crawl=new CrawlStat();
-    private final static Pattern FILTERS = Pattern.compile(".*(\\.(css|js|bmp|gif|jpe?g" 
+private final static Pattern FILTERS = Pattern.compile(".*(\\.(css|js|bmp|gif|jpe?g" 
                                                       + "|png|tiff?|mid|mp2|mp3|mp4"
                                                       + "|wav|avi|mov|mpeg|ram|m4v|pdf" 
                                                       + "|rm|smil|wmv|swf|wma|zip|rar|gz))$");
-   /**
-     * You should implement this function to specify whether
-     * the given url should be crawled or not (based on your
-     * crawling logic).
-     */
+   
+	//skip URLs containing certain characters as probable queries, etc.
+	private final static Pattern QUERRFILTERS = Pattern.compile(".*[\\?@=].*");
+		
+    //Begin with http://
+	private final static Pattern BEGIN = Pattern.compile("^http://.*\\.ics\\.uci\\.edu/.*");
+	
+	//exclude http://ftp file
+	//exclude http://fano.ics.uci.edu: Fano Experimental data
+	//exclude http://kdd.ics.uci.edu: UCI Knowledge Discovery in Databases
+	private final static Pattern EXCLUDE = Pattern.compile("^http://(ftp|fano|kdd)\\.ics\\.uci\\.edu/.*");
+
+    
     @Override
     public boolean shouldVisit(WebURL url) {
             String href = url.getURL().toLowerCase();
-                        return !FILTERS.matcher(href).matches() 
+			return !FILTERS.matcher(href).matches()
+					&& !QUERRFILTERS.matcher(href).matches()
+					&& BEGIN.matcher(href).matches()
+					//= href.startsWith("http://www.ics.uci.edu/");
+					&& !EXCLUDE.matcher(href).matches()
             		&&href.contains("ics.uci.edu")
             		&&!href.contains("archive.ics.uci.edu/ml/datasets.html")
             		&&!href.contains("calendar.ics.uci.edu")
             		&&!href.contains("djp3-pc2.ics")
             		&&!href.contains("drzaius.ics.uci.edu")
+            		&&!href.contains("theory.physics.uci.edu/WebCalendar")
+                    &&!href.contains("mine1.ics.uci.edu")
             		&&!crawl.fre_url.containsKey(url.getURL());
+
     }
     
     @Override
